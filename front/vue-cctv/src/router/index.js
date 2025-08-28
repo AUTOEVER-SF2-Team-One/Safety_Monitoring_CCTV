a8e7257fc0c5281bb151d98c5023a32c927a72fd
MillionKiwi <leehh41563636@gmail.com>
2025-08-28 11:10:23 +0900

    feat: Use alias in index.js

--- front/vue-cctv/src/router/index.js
+++ front/vue-cctv/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WorkerManagementView from '../views/WorkerManagementView.vue'
+import WorkerManagementView from '@/views/WorkerManagementView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
